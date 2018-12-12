ebe946472394d294888d1c954f57ef1e15016f6c
vyd <vyd2@uci.edu>
2018-12-12 13:53:44 -0800

    fix more typos

--- clustering.py
+++ clustering.py
@@ -2,19 +2,19 @@
 from __future__ import print_function
 import mdtraj as md
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
 import numpy as np
-from scipy.cluster.vq import kmeans
-from scipy.spatial.distance import cdist,pdist
-from sklearn import datasets
+from itertools import compress
+import pandas as pd
+import multiprocessing as mp
 import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn import mixture 
+from sklearn import datasets
 import scipy.stats
-from itertools import compress
-import pandas as pd
+from scipy.cluster.vq import kmeans
+from scipy.spatial.distance import cdist,pdist
 
-
+# visualization settings using seaborn 
 sns.set(font_scale=2) 
 sns.set_style("ticks")
 sns.set_palette("husl")
@@ -26,10 +26,9 @@ import time
 
 s_time = time.time()
 
-def get_dihedrals(top,traj):
-    '''
-    Get dihedral angles
-    '''
+def get_dihedrals(top,traj,og_list):
+    """ Get dihedral angles
+    """
 
     phi_list=[]
     psi_list=[]
@@ -40,22 +39,22 @@ def get_dihedrals(top,traj):
     # align frames to the first frame
     t.superpose(t, 0)
 
-    exec("ind,phi = md.compute_phi(t%s)" %i)
-    exec("ind2,psi = md.compute_psi(t%s)" %i)
+    ind,phi = md.compute_phi(t)
 
     phi_list.append(phi)
     phi_list.append(phi)
 
-    X = [np.concatenate(x) for index, x in enumerate(zip(np.concatenate(phio),np.concatenate(psio))) 
-    if index in og_list]
+    X = [np.concatenate(x) 
+         for index, x in enumerate(zip(np.concatenate(phio),np.concatenate(psio))) 
+         if index in og_list]
 
     return phi,psi,X
 
 
 def PCA_reduction(X):
-    '''
-    X = merged phi and psi dataset 
-    '''
+    """X = merged phi and psi dataset 
+    """
+    
 
     # PCA
     pca = PCA(0.99,whiten=True)
@@ -88,47 +87,56 @@ def PCA_reduction(X):
 
     return cIdx,gmm,X2,bic_min
 
-def get_cluster_centers(cIdx,gmm,X2,bic_min):
+def get_cluster_centers(cIdx,gmm,X2,bic_min,og_list):
+    """Use this function to get cluster centers from gmm model
+    """
 
     ids =[]
     rep_c =[]
     avgdist =[]
     perc_fr =[]
     
-    # representative of clusters
+    # representative clusters
     centers = np.empty(shape=(gmm.n_components, X2.shape[1]))
 
+    # choose data pt. w/max density 
+    # within each cluster/component
     for i in range(gmm.n_components):
-       density = scipy.stats.multivariate_normal(cov=gmm.covariances_[i], mean=gmm.means_[i]).logpdf(X2)
-        centers[i, :] = X2[np.argmax(density)]
+        density = scipy.stats.multivariate_normal(cov=gmm.covariances_[i], 
+                                                  mean=gmm.means_[i]).logpdf(X2)
 
-    # calc. euc. dist. b/n cent and X2 array, each column represents dist. calc. for a singl
+    # calc. euc. dist. b/n cent and X2 array, 
+    # each column represents dist. calc. for a singl
     D_k = [cdist(X2, cent, 'euclidean') for cent in [centers]] 
 
-    # generate list of indices of minimum values of each ROW, for each euc. dist. for K=1..1
-    # analogous distances that correspond to cIdx
+    # generate list of indices of minimum values of each ROW, 
+    # for each euc. dist. for K=1..1 analogous distances 
+    # that correspond to cIdx
     dist = [np.min(D,axis=1) for D in D_k]
 
-    kIdx=bic_min # cluster number 
+    # ideal cluster number determined from BIC/AIC 
+    # in PCA_reduction function 
+    kIdx=bic_min 
     cluster = []
 
     for i in range(kIdx):
 
-        # get list of ones of bool values
+        # get list of indexes that mathc bool
         ind = (cIdx==i)          
 
         # to get matching indices into cluster
         cluster.append(list(compress(range(len(ind)), ind)))  
 
-         # x=index, and corresponding index, each lst_c is new, with dist from cen
-        lst_c = { x : dist[0][x] for x in cluster[i]}     
+        # x=index, and corresponding index, each lst_c is new, with dist from cen
+        lst_c = {x: dist[0][x] for x in cluster[i]}     
       
-          # get min. val from dictionary lst_c, and gives the index of the value in the list 
+        # get min. val from dictionary lst_c, and gives the index of the value in the list 
         rep = min(lst_c, key=lst_c.get)     
 
         # get avg. dist. in each cluster
         Avgdist = np.mean(lst_c.values())   
 
+        # 100000 = total # of frames
         perc = len(cluster[i])/100000
         
         rep_c.append(all_perc[rep]+1)
